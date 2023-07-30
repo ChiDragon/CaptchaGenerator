0632f1691c090d615038cd1d192508ab992fc4fe
DarkDev <DarkDev@PC-VM-DEV2023>
2023-07-30 22:22:49 +0800

    [Dev]建立驗證碼-完成

--- CaptchaGenerator.WebSite/ClientApp/src/components/CaptchaGenerator.vue
+++ CaptchaGenerator.WebSite/ClientApp/src/components/CaptchaGenerator.vue
@@ -1,55 +1,82 @@
 <script setup>
-    import { ref, onMounted } from 'vue';
+    import { ref, reactive, onMounted } from 'vue';
 
     import DrawingSVG from '../components/DrawingSVG.vue'
     //import DrawingSVG from '../components/DrawingSVG.vue'
 
-    const msg = ref('TestMsg')
-    onMounted(() => {
-        //generate();
-    })
-
-    let captcha;
-    const generate = () => {
-
-        // Clear old input
-        document.getElementById("submit").value = "";
-
-        // Access the element to store
-        // the generated captcha
-        captcha = document.getElementById("image");
+    const generate = (number = 10) => {
         let uniquechar = "";
 
         const randomchar =
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            //"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-        // Generate captcha for length of
-        // 5 with random character
-        for (let i = 1; i <= 5; i++) {
+        for (let i = 1; i <= number; i++) {
             uniquechar += randomchar.charAt(
                 Math.random() * randomchar.length)
         }
 
-        // Store generated input
-        captcha.innerHTML = uniquechar;
+        return uniquechar;
+    }
+
+    const Captchas = reactive([])
+    onMounted(() => {
+        Captchas.push({
+            text: generate(8)
+        });
+    })
+
+    const inputCaptchaText = ref('');
+    const AddCaptcha = () => {
+        let msg = inputCaptchaText.value;
+        if (!msg) {
+            msg = '請輸入'; 
+        }
+        Captchas.push({
+            text: msg
+        });
+    }
+
+    const inputCaptchaNumber = ref(8);
+    const AddRandomCaptcha = () => {
+        Captchas.push({
+            text: generate(inputCaptchaNumber.value)
+        });
+    }
+
+    const DelCaptcha = (idx) => {
+        Captchas[idx].isDel = true;
     }
 
 </script>
 
 <template>
     <div class="captcha_generator_content">
-        <h1>建立驗證碼圖</h1>
+        <h1>建立驗證碼</h1>
         <div class="panel_body">
             <div>
-                <span>Test：</span>
-                <pre>{{ msg }}</pre>
+                <div style="margin-bottom:10px;">
+                    <button @click="AddRandomCaptcha">加入隨機驗證碼</button>
+                    <input type="number" @focus="$event.target.select()"
+                           v-model="inputCaptchaNumber" style="margin-left:10px;" />
+                </div>
+                <div style="margin-bottom:10px;">
+                    <button @click="AddCaptcha">加入指定內容驗證碼</button>
+                    <input placeholder="指定內容"
+                           v-model="inputCaptchaText" style="margin-left:10px;"/>
+                </div>
             </div>
             <hr />
-            <DrawingSVG :text="'Dark'" />
-            <!--<hr />
-            <DrawingSVG :text="'Test'" />
-            <hr />
-            <DrawingSVG :text="'YA!!'" />-->
+            <div v-for="(item,i) in Captchas">
+                <div v-if="!item.isDel">
+                    <div>
+                        <span>編號：{{i}}</span>
+                        <button @click="DelCaptcha(i)" style="margin-left:10px;">刪除</button>
+                    </div>
+                    <DrawingSVG :text="item.text" />
+                    <hr />
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -57,7 +84,7 @@
 <style scoped>
     .captcha_generator_content {
         width: 100%;
-        height: 600px;
+        /*height: 600px;*/
         padding: 4px;
         border: 1px solid red;
     }
@@ -66,5 +93,4 @@
             border: 1px solid #222;
             padding: 4px;
         }
-
 </style>
--- CaptchaGenerator.WebSite/ClientApp/src/components/DrawingSVG.vue
+++ CaptchaGenerator.WebSite/ClientApp/src/components/DrawingSVG.vue
@@ -1,43 +1,188 @@
+<script>
+    class PathPoint {
+        constructor(type, points = []) {
+            this.Type = type;
+            this.Points = points;
+        }
+    }
+
+    function getRandomId(number = 10) {
+        let rs = '';
+        let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+        for (var i = 0; i < number; i++) {
+            rs += alphabet[getRandomInt(alphabet.length)];
+        }
+        return rs;
+    }
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    function getNy(sy = 0, minY = 20, maxY = 80, dySize = 50, dyRange = 20) {
+        let ny = sy;
+        let dy = dySize - [...Array(dyRange).keys()][getRandomInt(dyRange)];
+        let tof = [true, false][getRandomInt(2)];
+        if ((sy < (minY + 10))) {
+            ny += dy;
+            if (ny > maxY) ny = maxY;
+            //console.log('往下')
+        } else if ((sy > (maxY - 10))) {
+            ny -= dy;
+            if (ny < minY) ny = minY;
+            //console.log('往上')
+        } else if (tof) {
+            ny += dy;
+            if (ny > maxY) ny = maxY;
+            //console.log('往下')
+        } else {
+            ny -= dy;
+            if (ny < minY) ny = minY;
+            //console.log('往上')
+        }
+        return ny;
+    }
+</script>
+
 <script setup>
     import { ref, reactive, onMounted, computed } from 'vue';
+    import * as htmlToImage from 'html-to-image';
+    import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
 
     const myProps = defineProps({
         text: String
     });
-    const CaptchaText = reactive(myProps.text)     
+    const CaptchaText = reactive(myProps.text)
 
+    const myId = ref('');
     onMounted(() => {
-        //const downloadSVG = document.querySelector('#downloadSVG');
-        //downloadSVG.addEventListener('click', downloadSVGAsText);
-        //const downloadPNG = document.querySelector('#downloadPNG');
-        //downloadPNG.addEventListener('click', downloadSVGAsPNG);
+        myId.value = getRandomId();
 
-        drawText();
+        generatorSvgPath([20, 40, 60, 80][getRandomInt(4)]);
+        setPathLength();
+        setFont();
+
+        setSvgToImg();
     })
 
+    const CaptchaImg = ref();
+    const SVG_NS = "http://www.w3.org/2000/svg";
     const CaptchaSvg = ref();
-    const obj = reactive({
-        props: {
-            x: 50,
-            y: 80,
-            "class": "CaptchaText",
-        }
-    });
+    const showCaptchaSvg = ref(true);
 
-    const SVG_NS = reactive("http://www.w3.org/2000/svg");
-    const drawText = () => {
-        let o = obj;
-        let svgNode = CaptchaSvg.value;
+    const DarkPath = ref();
+    const DarkTextPath = ref();
+    const generatorSvgPath = (startY = 30) => {
+        let pathPoints = [];
+        let xNodeCount = [3, 4][getRandomInt(2)];
+        let define_types = ['L', 'Q'];
+        //let define_types = ['C', 'Q'];
+        //let define_types = ['L', 'C', 'Q'];
 
-        let textNode = document.createElementNS(SVG_NS, "text");
-        for (var name in o.props) {
-            if (o.props.hasOwnProperty(name)) {
-                textNode.setAttributeNS(null, name, o.props[name]);
+        let sx = 20;
+        let sy = startY - [...Array(15).keys()][getRandomInt(15)];
+        let nx = sx;
+        let ny = sy;
+        for (var i = 1; i <= xNodeCount; i++) {
+            if (i == 1) {
+                pathPoints.push(new PathPoint('M', [[sx, sy]]));
+            } else {
+
+                let dx = (250 - nx) * Math.random();
+
+                let minDx = 30;
+                if (dx < minDx) { dx = minDx; }
+                let maxDx = (250 / (xNodeCount - 1));
+                if (dx > maxDx) { dx = maxDx; }
+
+                if (i == xNodeCount) {
+                    dx = (250 - nx);
+                }
+
+                let rt = define_types[getRandomInt(2)];
+                if (rt == 'L') {
+                    nx += dx;
+                    ny = getNy(ny);
+                    pathPoints.push(new PathPoint(rt, [[nx, ny]]));
+                } else if (rt == 'C') {
+                    let arr = [];
+
+                    let r = Math.random();
+                    let ddx = dx * r;
+                    let nnx = nx + ddx;
+                    ny = getNy(ny);
+                    arr.push([nnx, ny])
+
+                    r = Math.random();
+                    let dddx = (dx - ddx) * r;
+                    let nnnx = nnx + dddx;
+                    ny = getNy(ny);
+                    arr.push([nnnx, ny])
+
+                    nx += dx;
+                    ny = getNy(ny);
+                    arr.push([nx, ny])
+                    pathPoints.push(new PathPoint(rt, arr));
+                } else if (rt == 'Q') {
+                    let arr = [];
+
+                    let r = Math.random();
+                    let ddx = dx * r;
+                    let nnx = nx + ddx;
+                    ny = getNy(ny);
+                    arr.push([nnx, ny])
+
+                    nx += dx;
+                    ny = getNy(ny);
+                    arr.push([nx, ny])
+                    pathPoints.push(new PathPoint(rt, arr));
+                }
             }
         }
-        textNode.textContent = CaptchaText
-        svgNode.appendChild(textNode);
+        //console.log(`pathPoints:${pathPoints}`);
+        //console.log(pathPoints);
+
+        let d = '';
+        pathPoints.forEach((e) => {
+            d += ` ${e.Type}`;
+
+            e.Points.forEach((e2) => {
+                d += ` ${e2.toString()}`
+            });
+        });
+
+        //console.log(`d:${d}`);
+        let path = DarkPath.value;
+        path.setAttribute('d', d);
     }
+
+    const setPathLength = () => {
+        let path = DarkPath.value;
+        let len = path.getTotalLength() - 20;
+        let textPath = DarkTextPath.value;
+        textPath.setAttribute('textLength', len);
+    };
+
+    const myFont = ref();
+    const myFont_20 = computed(() => { return `${myFont.value}`.replaceAll(" ", "%20"); });
+    /*
+    ** 字形來源
+    ** https://fonts.google.com/?hl=zh-tw&preview.text=earhrss&preview.text_type=custom
+    */
+    const myFonts = ['Roboto', 'Borel', 'Oswald', 'Handjet',
+        'Edu SA Beginner', 'Lumanosimo', 'Tektur', 'Dancing Script',
+        'Maven Pro', 'EB Garamond', 'Teko', 'Lobster', 'Lugrasimo',
+        'Pacifico', 'Rajdhani', 'Caveat', 'Abril Fatface', 'Bungee Spice', 'Shadows Into Light',
+        'Indie Flower', 'Lilita One', 'Satisfy', 'Permanent Marker', 'Great Vibes', 'Yellowtail',
+        'Kaushan Script', 'Gloria Hallelujah', 'Sacramento', 'Big Shoulders Inline Text', 'Press Start 2P', 'Kablammo',
+        'Special Elite', 'Zeyada', 'Advent Pro', 'Tangerine', 'Bagel Fat One', 'Orbit',
+        'Moirai One', 'Marck Script', 'Bangers', 'Bungee', 'Allura', 'Neucha',
+        'Tsukimi Rounded', 'Playball', 'Monoton', 'Cherry Bomb One', 'Chokokutai', 'Homemade Apple',
+        'Six Caps', 'Rock Salt', 'Alex Brush', 'Black Ops One', 'Leckerli One', 'Julee',
+        'Covered By Your Grace', 'Rubik Moonrocks', 'Yesteryear', 'Rye', 'Henny Penny', 'Rubik Pixels'];
+    const setFont = () => {
+        myFont.value = myFonts[getRandomInt(myFonts.length)];
+    };
 
     const downloadSVGAsText = () => {
         const svg = CaptchaSvg.value;
@@ -68,7 +213,6 @@
             const dataURL = canvas.toDataURL('image/png');
             if (window.navigator.msSaveBlob) {
                 window.navigator.msSaveBlob(canvas.msToBlob(), "download.png");
-                e.preventDefault();
             } else {
                 const a = document.createElement('a');
                 const my_evt = new MouseEvent('click');
@@ -80,49 +224,121 @@
         }
     }
 
+    const downloadSVGAsPNG2 = () => {
+        var node = CaptchaSvg.value;
+
+        htmlToImage.toPng(node)
+            .then(function (dataUrl) {
+                showCaptchaSvg.value = true;
+
+                const a = document.createElement('a');
+                const my_evt = new MouseEvent('click');
+                a.download = `${CaptchaText}.png`;
+                a.href = dataUrl;
+                a.dispatchEvent(my_evt);
+            })
+            .catch(function (error) {
+                console.error('oops, something went wrong!', error);
+            });
+    };
+
+    const setSvgToImg = () => {
+        var svg = CaptchaSvg.value;
+        var img = CaptchaImg.value;
+
+        htmlToImage.toPng(svg)
+            .then(function (dataUrl) {
+                img.src = dataUrl;
+                showCaptchaSvg.value = false;
+            })
+            .catch(function (error) {
+                console.error('oops, something went wrong!', error);
+            });
+    };
+
+    const downloadCaptchaPNG = (fileName) => {
+        var img = CaptchaImg.value;
+        var fn = fileName;
+        if (!fn) {
+            fn = CaptchaText;
+        }
+
+        const a = document.createElement('a');
+        const my_evt = new MouseEvent('click');
+        a.download = `${fn}.png`;
+        a.href = img.src;
+        a.dispatchEvent(my_evt);
+    };
+
+    const inputCaptchaText = ref('');
+    const verifyRs = ref('');
+    const verifySpan = ref();
+    const verify = () => {
+        console.log(inputCaptchaText);
+        if (inputCaptchaText.value == CaptchaText) {
+            verifyRs.value = '驗證成功';
+            verifySpan.value.style.color = 'green';
+        } else {
+            verifyRs.value = '驗證失敗';
+            verifySpan.value.style.color = 'red';
+        }
+    };
+
+    const copyCaptchaText = () => {
+        navigator.clipboard.writeText(CaptchaText)
+            .then(() => {
+                let msg = '複製成功，請使用Ctrl + V';
+                verifyRs.value = msg;
+                //alert(msg);
+            })
+    }
 </script>
 
 <template>
-    <svg ref="CaptchaSvg"
+    <svg ref="CaptchaSvg" v-if="showCaptchaSvg"
          :xmlns="SVG_NS"
-         x="0px" y="0px" width="300px" height="100px"
+         x="0px" y="0px" width="600px" height="200px"
          viewBox="0 0 300 100">
 
-        <path id="MyPath"
-              fill="none"
-              stroke="red"
-              d="M10,90 Q90,90 90,45 Q90,10 50,10 Q10,10 10,40 Q10,70 45,70 Q70,70 75,50" />
-
         <component :is="'style'">
-            .small {
-            font: italic 13px sans-serif;
-            }
-
-            .heavy {
-            font: bold 30px sans-serif;
-            }
-
-            .Rrrrr {
-            font: italic 40px serif;
-            fill: red;
-            }
-
-            .CaptchaText {
-            font: italic 40px serif;
-            fill: blue;
-            }
+            @import url(https://fonts.googleapis.com/css?family={{myFont_20}});
         </component>
 
+        <path ref="DarkPath" :id="myId" stroke="red" fill="none"
+              d="M0,100 L100,0 L200,100 L300,0" />
+
         <text>
-            <textPath href="#MyPath">Quick brown fox jumps over the lazy dog.</textPath>
+            <textPath ref="DarkTextPath"
+                      :class="`CaptchaText_`+myId"
+                      :href="`#`+myId"
+                      href="#DarkPath"
+                      startOffset="10">
+                {{CaptchaText}}
+            </textPath>
         </text>
 
-        <text x="20" y="35" class="small">My</text>
-        <text x="40" y="35" class="heavy">cat</text>
-        <text x="55" y="55" class="small">is</text>
-        <text x="65" y="55" class="Rrrrr">Grumpy!</text>
+        <component :is="'style'">
+            .CaptchaText_{{myId}} {
+            fill: blue;
+            font-family: '{{myFont}}', serif;
+            <!--font-size: 24px;-->
+            }
+        </component>
     </svg>
+    <div>
+        <img ref="CaptchaImg" />
+    </div>
     <br />
-    <button @click="downloadSVGAsPNG">Download .png</button>
-    <button @click.prevent="downloadSVGAsText">Download .svg</button>
+    <div style="margin-bottom: 10px;">
+        <span style="margin-left:10px;">輸入驗證碼</span>
+        <input v-model="inputCaptchaText" @focus="$event.target.select()"
+               v-on:change="verify" style="margin-left:10px;" />
+        <span ref="verifySpan" style="margin-left: 10px;">{{verifyRs}}</span>
+    </div>
+    <!--<button @click="downloadSVGAsPNG2">下載圖片</button>-->
+    <!--<button @click="downloadSVGAsPNG2">下載圖片(檔名不同步)</button>-->
+    <button @click.prevent="downloadCaptchaPNG()" style="margin-left: 10px;">下載圖片</button>
+    <button @click="downloadCaptchaPNG('驗證碼')" style="margin-left: 10px;">下載圖片(檔名不同步)</button>
+    <button @click="copyCaptchaText" style="margin-left: 10px;">複製驗證碼</button>
+    <br />
 </template>
